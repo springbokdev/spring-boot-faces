54bd3bed24002d4a7c017ccca51fbd930148a1d9
johnspangenberg <dev@springbok.space>
2024-07-04 14:38:23 +0200

    Upgrade to latest Spring Boot version.

--- src/main/java/com/auth0/samples/bootfaces/Application.java
+++ src/main/java/com/auth0/samples/bootfaces/Application.java
@@ -2,7 +2,7 @@ package com.auth0.samples.bootfaces;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
