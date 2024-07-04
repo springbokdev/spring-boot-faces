f899c1515bc4ee87bb61c0c46a326898a908f0cf
johnspangenberg <dev@springbok.space>
2024-07-04 15:53:15 +0200

    This sucks.

--- src/main/java/com/auth0/samples/bootfaces/Application.java
+++ src/main/java/com/auth0/samples/bootfaces/Application.java
@@ -1,15 +1,48 @@
 package com.auth0.samples.bootfaces;
 
+import jakarta.servlet.DispatcherType;
+import org.ocpsoft.rewrite.servlet.RewriteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration
-@ComponentScan({"com.auth0.samples.bootfaces"})
-public class Application extends SpringBootServletInitializer {
+import javax.faces.webapp.FacesServlet;
+import java.util.EnumSet;
+
+//@EnableAutoConfiguration
+//@ComponentScan({"com.auth0.samples.bootfaces"})
+//public class Application extends SpringBootServletInitializer {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(Application.class, args);
+//	}
+//}
+
+@SpringBootApplication
+public class Application  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public ServletRegistrationBean servletRegistrationBean() {
+		FacesServlet servlet = new FacesServlet();
+		return new ServletRegistrationBean(servlet, "*.jsf");
+	}
+
+	@Bean
+	public FilterRegistrationBean rewriteFilter() {
+		FilterRegistrationBean rwFilter = new FilterRegistrationBean(new RewriteFilter());
+
+		rwFilter.setDispatcherTypes(EnumSet.of(DispatcherType.FORWARD, DispatcherType.REQUEST,
+			DispatcherType.ASYNC, DispatcherType.ERROR));
+		rwFilter.addUrlPatterns("/*");
+		return rwFilter;
 	}
 }
